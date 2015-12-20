122383fbfd82ce99de171f56b261377351ffc68a
lovebube <lovebube88@gmail.com>
2015-12-20 23:32:44 +0900

    Initial Commit

---
+++ src/kr/ac/uos/ai/annotator/Application.java
@@ -0,0 +1,13 @@
+package kr.ac.uos.ai.annotator;
+
+/**
+ * @author Chan Yeon, Cho
+ * @version 0.0.1 - Snapshot
+ *          on 2015-12-20
+ * @link http://github.com/lovebube
+ */
+public class Application {
+
+    public Application() {
+    }
+}
